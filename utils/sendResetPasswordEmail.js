3785fde2fc673a49cacc5a8c1a79530e59dc0480
johnemad1 <103380837+johnemad1@users.noreply.github.com>
2024-06-05 16:59:35 +0300

    Update sendResetPasswordEmail.js

--- utils/sendResetPasswordEmail.js
+++ utils/sendResetPasswordEmail.js
@@ -1,16 +1,27 @@
 const sendEmail = require('./sendEmail');
 
+// const sendResetPassswordEmail = async ({ name, email, token, origin }) => {
+//   const resetURL = `${origin}/user/reset-password?token=${token}&email=${email}`;
+//   const message = `<p>Please reset password by clicking on the following link : 
+//   <a href="${resetURL}">Reset Password</a></p>`;
+
+//   return sendEmail({
+//     to: email,
+//     subject: 'Reset Password',
+//     html: `<h4>Hello, ${name}</h4>
+//    ${message}
+//    `,
+//   });
+// };
+
 const sendResetPassswordEmail = async ({ name, email, token, origin }) => {
   const resetURL = `${origin}/user/reset-password?token=${token}&email=${email}`;
-  const message = `<p>Please reset password by clicking on the following link : 
-  <a href="${resetURL}">Reset Password</a></p>`;
+  const message = `<p>Please reset your password by clicking on the following link: <a href="${resetURL}">Reset Password</a></p>`;
 
   return sendEmail({
     to: email,
     subject: 'Reset Password',
-    html: `<h4>Hello, ${name}</h4>
-   ${message}
-   `,
+    html: `<h4>Hello, ${name}</h4>${message}`,
   });
 };
 
