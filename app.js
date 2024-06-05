6dde6a184e19077bb394de9ae60f1ce5312cde78
johnemad1 <103380837+johnemad1@users.noreply.github.com>
2024-06-05 19:18:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -38,11 +38,10 @@ const schedulesRouter = require('./routes/scheduleRoutes');
 
 app.use(
   cors({
-    const corsOptions = {
     origin: 'https://faculty-of-applied-arts.netlify.app', // your frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-};
+
     // origin: 'http://localhost:3000',
     // credentials: true,
   }),
